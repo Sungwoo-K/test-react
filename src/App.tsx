5e38c20a1a9ff17552a2b60435d683c129a9700e
Sungwoo-K <72499989+Sungwoo-K@users.noreply.github.com>
2023-10-25 15:10:49 +0900

    Test react slick

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
+import SimpleSlider from "./test";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 const App = () => {
-  return <div></div>;
+  return <SimpleSlider></SimpleSlider>;
 };
 
 export default App;
